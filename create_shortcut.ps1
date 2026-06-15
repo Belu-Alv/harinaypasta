6bd86f6e07e79fd21ed75d92d74d1ced4f59f00b
Belu-Alv <joalvareck@gmail.com>
2026-06-15 11:01:05 -0300

    Update create_shortcut.ps1

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -1,7 +1,7 @@
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$desktopPath\Gourmet Hub.lnk")
-$Shortcut.TargetPath = "C:\Users\usuario\.gemini\antigravity\scratch\delivery-whatsapp-app\index.html"
+$Shortcut.TargetPath = "file:///C:/Users/harina%20y%20salsa/delivery-whatsapp-app/index.html"
 $Shortcut.Description = "Gourmet Hub | Delivery & Admin Dashboard"
 $Shortcut.Save()
 Write-Output "Shortcut created successfully at: $desktopPath\Gourmet Hub.lnk"
